4d87998b0176042523c5a8602d1eba3e896057f3
Andreas Heiberg <heibergandreas@gmail.com>
2013-06-20 21:28:53 +0200

    fixed indentation

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 $(function($){
-   $('a.nav').click(function() {
-        var $this = $(this),
-          _href = $this.attr('href'),
-	    	  dest  = $(_href).offset().top;
-            $("html:not(:animated),body:not(:animated)").animate({  scrollTop: dest}, 400 );
-  	    return false;
-    }); 
+	$('a.nav').click(function() {
+		var $this = $(this),
+		_href = $this.attr('href'),
+		dest  = $(_href).offset().top;
+
+		$("html:not(:animated),body:not(:animated)").animate({  scrollTop: dest}, 400 );
+		
+		return false;
+	}); 
 });
